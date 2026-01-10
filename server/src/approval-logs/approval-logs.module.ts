cf3603041ba27f5317cc6242dc141a7d15a3d448
Patil Kundan <kundanpatil4828@gmail.com>
2026-01-10 23:18:59 +0530

    feat: generate approval logs service

--- server/src/approval-logs/approval-logs.module.ts
+++ server/src/approval-logs/approval-logs.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApprovalLog } from './approval-log.entity';
+import { ApprovalLogsService } from './approval-logs.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ApprovalLog])],
+  providers: [ApprovalLogsService],
 })
 export class ApprovalLogsModule {}
